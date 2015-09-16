2fc5d6471d69594e839584bb9efc879ed0cfc0ad
abejfehr <abe.fehr@gmail.com>
2015-09-15 20:30:00 -0400

    Make addPlayer method work, test passes

--- src/Game.java
+++ src/Game.java
@@ -1,9 +1,21 @@
+import java.util.HashMap;
 
 public class Game {
 
-	private int numPlayers = 0;
+	private int numPlayers;
+	private HashMap<Integer, String> players;
+	
+	public Game() {
+		numPlayers = 0;
+		players = new HashMap<Integer, String>();
+	}
 	
 	public int getNumPlayers() {
 		return numPlayers;
+	}
+	
+	public void addPlayer(int playerId, String hand) {
+		players.put(playerId, hand);
+		++numPlayers;
 	}
 }
